f5a9e023972d274e5a82ec805fbff4f0f0ce3a1f
Trang Tran <trangham283@gmail.com>
2018-04-26 17:28:26 -0700

    Fixed more bugs in pair_to_singles conversion

--- src/tree_transform/pairs_to_singles.py
+++ src/tree_transform/pairs_to_singles.py
@@ -2,7 +2,7 @@
 from parse_analyzer import render_tree, init, treebanks, parse_errors, nlp_eval
 from parse_analyzer import pstree, transform_search, head_finder, tree_transform
 from tree_convert_utils import *
-import sys, argparse, os
+import sys, argparse, os, glob
 from parse_analyzer.classify_english import classify
 from collections import defaultdict
 from StringIO import StringIO
@@ -39,9 +39,15 @@ def align_pairs(file_num, speaker, ms_hypo_dir):
         turn = int(a.split('_')[1][1:])
         sent_num = int(a.split('_')[2])
         ms_side = [x[0] for x in pairs if (turn, sent_num) in x[0]]
-        ms_side = set([item for sublist in ms_side for item in sublist])
+        ms_side = [item for sublist in ms_side for item in sublist]
+        ms_side = [x for x in ms_side if \
+                make_sent_id(file_num, speaker,*x) in data['ms_sents']]
+        ms_side = set([x for x in ms_side if \
+                data['ms_sents'][make_sent_id(file_num, speaker,*x)][0]])
         ptb_side = [x[1] for x in pairs if (turn, sent_num) in x[0]]
-        ptb_side = set([item for sublist in ptb_side for item in sublist])
+        ptb_side = [item for sublist in ptb_side for item in sublist]
+        ptb_side = set([x for x in ptb_side if \
+                data['ptb_sents'][make_sent_id(file_num, speaker,*x)][0]])
         idx = is_in_set(ms_side, sides)
         if idx > -1:
             ptb_side = ptb_side.union(sides[idx][1])
@@ -50,6 +56,8 @@ def align_pairs(file_num, speaker, ms_hypo_dir):
             sides.append([ms_side, ptb_side])
     
     for ms_side, ptb_side in sides:
+        #print ms_side, ptb_side
+        if not ms_side or not ptb_side: continue
         ms_tokens = []
         offsets = []
         offset = 0
@@ -61,11 +69,15 @@ def align_pairs(file_num, speaker, ms_hypo_dir):
             offsets.append(offset)
             offset += len(this_tokens)
 
-        ptb_trees = []
+        ptb_trees = [] 
         i = 0
         for turn, sent_num in sorted(ptb_side):
             this_id = make_sent_id(file_num, speaker, turn, sent_num)
-            this_mrg = "(ROOT " + data['ptb_sents'][this_id][1][1:]
+            # sometimes PTB sentences starts with "(S" instead of "( "
+            if data['ptb_sents'][this_id][1][:2] == "(S":
+                this_mrg = "(ROOT (S "+data['ptb_sents'][this_id][1][2:]+")"
+            else:
+                this_mrg = "(ROOT " + data['ptb_sents'][this_id][1][1:]
             this_tree = pstree.tree_from_text(this_mrg)
             if i > 0:
                 this_tree = merge_trees(ptb_trees[i-1], this_tree)
@@ -89,9 +101,17 @@ def align_pairs(file_num, speaker, ms_hypo_dir):
                         replace_words(this_tree, ms_tokens, i1, i2, j1, j2)
                     # harder case: replace ptb_words[i1:i2] by ms_token[j1:j2]
                     else:
-                        delete_words(this_tree, i1, i2)
-                        this_tree = insert_words(this_tree, ms_tokens, i1, i2, \
-                                j1, j2)
+                        if (i2-i1) == len(ptb_tokens):
+                            # replacing the whole sentence
+                            to_insert = ms_tokens[j1:j2]
+                            str_mrg = ['(' + look_up_tag(w) + ' ' + w +')' \
+                                    for w in to_insert]
+                            mrg = '(ROOT (S ' + ' '.join(str_mrg) + ' ) )'
+                            this_tree = pstree.tree_from_text(mrg)
+                        else:
+                            delete_words(this_tree, i1, i2)
+                            this_tree = insert_words(this_tree, ms_tokens, \
+                                    i1, i2, j1, j2)
                 elif tag == 'delete':
                     delete_words(this_tree, i1, i2)
                 else:
@@ -135,19 +155,23 @@ def write_output(temp_mrg, data, out_name):
     df.to_csv(out_name, sep='\t', index=False, header=True, \
         columns=["sent_id", "ms_sent", "ptb_sent", "comb_ann", "times", "mrg"])
 
+# TEMP PATCH
+todo = open('check_files.txt').readlines()
+todo = [x.strip() for x in todo]
+todo = set([int(x.split()[0]) for x in todo])
 
 if __name__ == '__main__':
     pa = argparse.ArgumentParser(description = \
             'Merge/Split cases with mismatched sentence boundaries')
     pa.add_argument('--ms_hypo_dir', type=str, \
-            default='/s0/ttmt001/speech_parsing/prosodic-anomalies',\
+            default='/s0/ttmt001/speech_parsing/ms_ptb_info',\
             help='directory of ms hypotheses')
     pa.add_argument('--file_num', default=2005, type=int, \
             help='file number, 0 for all')
     pa.add_argument('--speaker', default='A', type=str, \
             help='speaker')
     pa.add_argument('--out_dir', type=str, \
-            default='/s0/ttmt001/speech_parsing/prosodic-anomalies', \
+            default='/s0/ttmt001/speech_parsing/ms_ptb_info', \
             help='output directory')
 
     # previously both ms_hypo_dir and out_dir were "samples"
@@ -158,9 +182,29 @@ if __name__ == '__main__':
     out_dir = args.out_dir
     ms_hypo_dir = args.ms_hypo_dir
 
-    temp_mrg, data = align_pairs(file_num, speaker, ms_hypo_dir)
-    out_name = os.path.join(out_dir, \
+    if file_num != 0:
+        temp_mrg, data = align_pairs(file_num, speaker, ms_hypo_dir)
+        out_name = os.path.join(out_dir, \
             "{0}_{1}_updated_pairs.tsv".format(file_num, speaker))
-    write_output(temp_mrg, data, out_name)
+        write_output(temp_mrg, data, out_name)
+    else:
+        all_files = glob.glob(ms_hypo_dir + "/*_pairs.pickle")
+        for f in all_files:
+            fname = os.path.basename(f).split('_')
+            file_num = int(fname[0])
+            # skip done files
+            if file_num not in todo: continue
+            speaker = fname[1]
+            print file_num, speaker
+            try:
+                temp_mrg, data = align_pairs(file_num, speaker, ms_hypo_dir)
+                out_name = os.path.join(out_dir, \
+                        "{0}_{1}_updated_pairs.tsv".format(file_num, speaker))
+                write_output(temp_mrg, data, out_name)
+            except:
+                print "Couldn't process file", file_num, speaker
+                continue
+
+
 
 
